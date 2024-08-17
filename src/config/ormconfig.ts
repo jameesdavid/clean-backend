f5e3623f421ffd079f3a2b16d6249d41bd0cfe8f
jameesdavid <jameesdavid@outlook.com.br>
2024-08-17 17:22:45 -0300

    Chore [DB]: change database to postgres

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -3,16 +3,21 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const ormConfig: TypeOrmModuleOptions = {
-  type: "mysql",
+  type: "postgres",
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   autoLoadEntities: true,
   synchronize: process.env.NODE_ENV === "development" ? true : false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   extra: {
-    ssl: false,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
 };
 
